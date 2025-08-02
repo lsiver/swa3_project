7eec8b7b8fe9cf0945656833cbd9907168e8838a
Luke Siverling <ldsiverling@gmail.com>
2025-08-01 22:05:13 -0500

    fixed a typo

--- backend/tasks.py
+++ backend/tasks.py
@@ -2,9 +2,10 @@ from celery import Celery
 from chem.DC.antoine_data_scraper import build_antoine_list_oneshot
 from chem.DA.bindist import BinDist
 import os
+from api import app
 
 redis_url = os.environ.get('REDIS_URL','redis://localhost:6379')
-celery = Celery('distillation_tasks', broker='redis_url')
+celery = Celery('distillation_tasks', broker=redis_url)
 
 @celery.task(bind=True)
 def scrape_antoine_data(self):
@@ -13,6 +14,6 @@ def scrape_antoine_data(self):
         build_antoine_list_oneshot()
         return {'status':'success','message':'Antoine data updated successfully'}
     except Exception as e:
-        if self.requests.retries < 3:
+        if self.request.retries < 3:
             raise self.retry(countdown = 60, exc=e)
         return {'status':'failed','error':str(e)}
