bb247cbf5f78cfd520ce0ea8304847486b63ce2d
Luke Siverling <ldsiverling@gmail.com>
2025-08-03 16:38:23 -0500

    added unit tests

---
+++ backend/test/DA/test_psat_calc.py
@@ -0,0 +1,10 @@
+from unittest import TestCase
+
+from chem.DA.psat_calc import psat_calculation
+
+
+class TestPsatCalc(TestCase):
+    def test_psat_calc(self) -> None:
+        psat_toluene = psat_calculation("Toluene")
+        psat_benzene = psat_calculation("Benzene")
+        assert((0.7 <= psat_toluene <= 0.8) and (1.7 <= psat_benzene <= 1.9))
